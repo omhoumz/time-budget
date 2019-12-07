e4ae6c196729378504ace7effce436e449393e5f
Omar Houmz <omarhoumz@gmail.com>
2019-12-07 18:58:45 +0100

    chore: extract the copyelement

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -3,23 +3,21 @@ import React from 'react'
 import Wrapper from '../ui/wrapper/wrapper'
 import styles from './footer.module.css'
 
+const copyText = `© ${new Date().getFullYear()}, Built with `
+
+const byAuthor = ` by `
+
+export const copyElement = (
+  <p className={styles.copy}>
+    {copyText}
+    <a href='https://www.gatsbyjs.org'>Gatsby</a>
+    {byAuthor}
+    <a href='https://github.com/omhoumz'>Omar Houmz</a>
+  </p>
+)
+
 const Footer = () => {
-  const copyText = `© ${new Date().getFullYear()}, Built with `
-
-  const byAuthor = ` by `
-
-  return (
-    <footer className={styles.footer}>
-      <Wrapper>
-        <p className={styles.copy}>
-          {copyText}
-          <a href='https://www.gatsbyjs.org'>Gatsby</a>
-          {byAuthor}
-          <a href='https://github.com/omhoumz'>Omar Houmz</a>
-        </p>
-      </Wrapper>
-    </footer>
-  )
+  return <footer className={styles.footer}>{copyElement}</footer>
 }
 
 export default Footer
