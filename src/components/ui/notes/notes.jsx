67acfcf060ef9d5a0453d63c26b557c8a13aaee7
Omar Houmz <omarhoumz@gmail.com>
2019-12-10 22:27:50 +0100

    feat: notes redesign & add time

--- src/components/ui/notes/notes.jsx
+++ src/components/ui/notes/notes.jsx
@@ -3,11 +3,23 @@ import PropTypes from 'prop-types'
 
 import styles from './notes.module.css'
 
-const Notes = ({ messages }) => {
-  return <div className={styles.notes}>{messages.map(note)}</div>
+const Notes = ({ messages, date }) => {
+  return (
+    <div className={styles.notesWrapper}>
+      <div className={styles.timeColumn}>
+        <h3 className={styles.notesTime}>{date}</h3>
+        <button className={styles.addBtn}>+</button>
+      </div>
+      <div className={styles.notes}>{messages.map(note)}</div>
+    </div>
+  )
 }
 
-const note = ({ message }) => <div className={styles.note}>{message}</div>
+const note = ({ message }, index) => (
+  <div className={styles.note} key={index}>
+    {message}
+  </div>
+)
 
 Notes.propTypes = {
   messages: PropTypes.array.isRequired,
