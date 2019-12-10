238b34a86bf1898205aaff0182e53cbc8ed5a68e
Omar Houmz <omarhoumz@gmail.com>
2019-12-10 22:28:10 +0100

    chore: refactor app page to use notes & sidebar

--- src/page-contents/app-page/app-page.jsx
+++ src/page-contents/app-page/app-page.jsx
@@ -1,33 +1,10 @@
 import React from 'react'
 import cx from 'classnames'
-import { Link } from 'gatsby'
 
 import SEO from '../../components/seo/seo'
 import styles from './app-page.module.css'
-import { copyElement } from '../../components/footer/footer'
 import Notes from '../../components/ui/notes/notes'
-
-const navItemsContent = [
-  { label: 'weekday', link: '/app#weekday' },
-  { label: 'weekend', link: '/app#weekend' },
-]
-
-const linkItemsContent = [
-  { label: 'about', link: '/about' },
-  { label: 'help', link: '/help' },
-]
-
-const navItem = ({ label, link }) => (
-  <Link to={link} className={styles.navItem}>
-    {label}
-  </Link>
-)
-
-const linkItem = ({ label, link }) => (
-  <Link to={link} className={styles.linkItem}>
-    {label}
-  </Link>
-)
+import SideBar from '../../components/side-bar/side-bar'
 
 const messages = [{ message: '1' }, { message: '2' }, { message: '3' }]
 
@@ -35,20 +12,13 @@ const AppPage = () => {
   return (
     <main className={cx('main', styles.main)}>
       <SEO title='App' />
-      <div className={styles.side}>
-        <div className={styles.nav}>
-          <Link className={styles.logo}>
-            time<strong>Budget</strong>
-          </Link>
-          {navItemsContent.map(navItem)}
-        </div>
-        <div className={styles.links}>
-          {linkItemsContent.map(linkItem)}
-          {copyElement}
-        </div>
-      </div>
+      <SideBar />
       <div className={styles.content}>
-        <Notes messages={messages} />
+        <Notes date='08:00pm' messages={messages} />
+        <Notes date='09:00pm' messages={messages} />
+        <Notes date='10:00pm' messages={messages} />
+        <Notes date='11:00pm' messages={messages} />
+        <Notes date='00:00am' messages={messages} />
       </div>
     </main>
   )
